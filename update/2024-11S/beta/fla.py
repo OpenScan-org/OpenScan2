f5e3562a3309a2de94b1aaa150f7df42b23b2824
Stealth <stealth@MacBook-Pro-de-Stealth.local>
2024-09-15 17:03:08 +0200

    add swagger

---
+++ update/2024-11S/beta/fla.py
@@ -0,0 +1,351 @@
+from flask import Flask, make_response, jsonify, request, abort, redirect
+from picamera2 import Picamera2
+from PIL import Image, ImageDraw, ImageOps, ImageFilter, ImageEnhance, ImageChops, ImageFont
+from time import sleep, time
+import shutil
+from OpenScan import load_int, load_float, load_bool, ringlight
+import RPi.GPIO as GPIO
+from math import sqrt
+import os
+import math
+from skimage import io, feature, color, transform
+import numpy as np
+from scipy import ndimage
+import socket
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+app = Flask(__name__)
+
+basedir = '/home/pi/OpenScan/'
+timer = time()
+cam_mode = 0
+hostname = socket.gethostname().split(":")
+
+def overlay_mask(image, mask_image):
+    # Ensure image is in RGB mode
+    image_rgb = image.convert('RGB')
+    # Create an empty image with RGBA channels
+    overlay = Image.new('RGBA', image_rgb.size)
+
+    # Prepare a red image of the same size
+    red_image = Image.new('RGB', image_rgb.size, (255, 0, 0))
+    # Prepare a mask where the condition is met (mask_image pixels == 255)
+    mask_condition = np.array(mask_image) > 0
+    overlay_mask = Image.fromarray(np.uint8(mask_condition) * 255)
+    # Paste the red image onto the overlay using the condition mask
+    overlay.paste(red_image, mask=overlay_mask)
+    # Combine the original image with the overlay
+    combined = Image.alpha_composite(image_rgb.convert('RGBA'), overlay)
+    # Convert the final image to RGB
+    combined_rgb = combined.convert('RGB')
+    return combined_rgb
+
+
+
+def highlight_sharpest_areas(image, threshold=load_int('cam_sharpness'), dilation_size=5):
+    # Convert PIL image to grayscale
+    image_gray = image.convert('L')
+
+    # Convert grayscale image to numpy array
+    image_array = np.array(image_gray)
+
+    # Calculate the gradient using a Sobel filter
+    dx = ndimage.sobel(image_array, 0)  # horizontal derivative
+    dy = ndimage.sobel(image_array, 1)  # vertical derivative
+    mag = np.hypot(dx, dy)  # magnitude
+
+    # Threshold the gradient to create a mask of the sharpest areas
+    mask = np.where(mag > threshold, 255, 0).astype(np.uint8)
+
+    dilated_mask = ndimage.binary_dilation(mask, structure=np.ones((dilation_size,dilation_size)))
+    # Create a PIL image from the mask
+    mask_image = Image.fromarray(dilated_mask)
+
+    return mask_image
+
+
+
+
+###################################################################################################################
+@app.route('/shutdown', methods=['get'])
+def shutdown():
+    shutdown_token = request.args.get('token')
+    hostname = request.host.split(":")[0]
+    f = open("/home/pi/OpenScan/settings/session_token", "r")
+    session_token = (f.readline())[:20]
+    if shutdown_token == session_token:
+
+        delay = 0.1
+        ringlight(2,False)
+
+        for i in range (5):
+            ringlight(1,True)
+            sleep(delay)
+            ringlight(1,False)
+            sleep(delay)
+        os.system('shutdown -h now')
+
+    else:
+        return redirect("http://" + hostname, code=302)
+###################################################################################################################
+@app.route('/reboot', methods=['get'])
+def reboot():
+    shutdown_token = request.args.get('token')
+    hostname = request.host.split(":")[0]
+    f = open("/home/pi/OpenScan/settings/session_token", "r")
+    session_token = (f.readline())[:20]
+    if shutdown_token == session_token:
+        delay = 0.1
+        ringlight(2,False)
+
+        for i in range (5):
+            ringlight(1,True)
+            sleep(delay)
+            ringlight(1,False)
+            sleep(delay)
+
+        os.system('reboot -h')
+    else:
+        return redirect("http://" + hostname, code=302)
+###################################################################################################################
+
+def plot_orb_keypoints(pil_image):
+    downscale = 2
+    # Read the image from the given image path
+    image = np.array(pil_image)
+    #image = io.imread(image_path)
+    image = transform.resize(image, (image.shape[0] // downscale, image.shape[1] // downscale), anti_aliasing=True)
+
+    # Convert the image to grayscale
+    gray_image = color.rgb2gray(image)
+
+    try:
+        orb = feature.ORB(n_keypoints=10000, downscale=1.2, fast_n=2, fast_threshold=0.2 , n_scales=3, harris_k=0.001)
+        orb.detect_and_extract(gray_image)
+        keypoints = orb.keypoints
+    except:
+        return pil_image
+
+    # Convert the image back to the range [0, 255]
+    display_image = (image * 255).astype(np.uint8)
+
+    # Draw the keypoints on the image
+    draw = ImageDraw.Draw(pil_image)
+    size = max(2,int(image.shape[0]*downscale*0.005))
+    for i, (y, x) in enumerate(keypoints):
+        draw.ellipse([(downscale*x-size, downscale*y-size), (downscale*x+size, downscale*y+size)], fill = (0,255,0))
+    # Save the image with keypoints to the given output path
+    return pil_image
+
+def add_histo(img):
+    histo_size = 241
+
+    img_gray = ImageOps.grayscale(img)
+    histogram = img_gray.histogram()
+    histogram_log = [math.log10(h + 1) for h in histogram]
+    histogram_max = max(histogram_log)
+    histogram_normalized = [float(h) / histogram_max for h in histogram_log]
+    hist_image = Image.new("RGBA", (histo_size, histo_size), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(hist_image)
+
+    for i in range(0, 256):
+        x = i
+        y = 256 - int(histogram_normalized[i] * 256)
+        draw.line((x, 256, x, y), fill=(0, 0, 0, 255))
+
+    text = ""
+    if min(histogram[235:238])>0:
+        text = "overexposed"
+    if sum(histogram[190:192])<8:
+        text = "underexposed"
+    font = ImageFont.truetype("DejaVuSans.ttf", 30)
+
+    bbox = draw.textbbox((0, 0), text, font=font)
+
+    text_width = bbox[2] - bbox[0]
+    text_height = bbox[3] - bbox[1]
+
+
+    x = (hist_image.width - text_width )/2
+    y = hist_image.height - text_height - 10
+    draw.text((x, y), text, font=font, fill=(255,0,0))
+
+    scale = 0.25
+    width1, height1 = hist_image.size
+    width2 = img.size[0]
+    new_width1 = int(width2 * scale)
+    new_height1 = int((height1 / width1) * new_width1)
+    hist_image = hist_image.convert('RGB')
+
+    hist_image = hist_image.resize((new_width1, new_height1))
+    x = hist_image.width - text_width - 10
+    y = hist_image.height - text_height - 10
+
+
+    img.paste(hist_image, (img.size[0]-new_width1-int(0.01*img.size[0]),img.size[1]-new_height1-int(0.01*img.size[0])))
+
+    return img
+
+def create_mask(image: Image, scale: float = 0.1, threshold: int = 45) -> Image:
+    threshold = load_int("cam_mask_threshold")
+    if threshold <= 1:
+        return image
+    orig = image
+    image = image.resize((int(image.width*scale),int(image.height*scale)))
+    image = image.convert("L")
+    reduced = image
+    image = image.filter(ImageFilter.EDGE_ENHANCE)
+    image = image.filter(ImageFilter.BLUR)
+    reduced = reduced.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    mask = ImageChops.difference(image, reduced)
+    mask = ImageEnhance.Brightness(mask).enhance(2.5)
+    mask = mask.filter(ImageFilter.MaxFilter(9))
+    mask = mask.filter(ImageFilter.MinFilter(5))
+    mask = mask.point(lambda x: 255 if x <threshold else 0)
+    mask = mask.filter(ImageFilter.MaxFilter(5))
+    mask = mask.convert(orig.mode)
+    mask = mask.resize((orig.width,orig.height), resample=Image.BOX)
+    result = ImageChops.subtract(orig, mask)
+
+    return result
+
+###################################################################################################################
+@app.route('/picam2_init', methods=['get'])
+def picam2_init():
+    global picam2
+    global preview_config
+    global capture_config
+
+    try:
+        picam2.controls.AnalogueGain = 1.0
+        return ({}, 200)
+    except:
+        pass
+    picam2 = Picamera2()
+
+#    preview_config = picam2.create_preview_configuration(main={"size": (1280, 720)}) #--> wrong aspect ratio!
+#    preview_config = picam2.create_preview_configuration(main={"size": (2028, 1520)})
+    preview_config = picam2.create_preview_configuration(main={"size": (2028, 1520)}, controls ={"FrameDurationLimits": (1, 1000000)})
+
+#    preview_config = picam2.create_preview_configuration(main={"size": (2328, 1748)})
+    capture_config = picam2.create_still_configuration(controls ={"FrameDurationLimits": (1, 1000000)})
+    picam2.configure(preview_config)
+    picam2.controls.AnalogueGain = 1.0
+    picam2.start()
+    return ({}, 200)
+
+###################################################################################################################
+@app.route('/picam2_take_photo', methods=['get'])
+def picam2_take_photo():
+    starttime = time()
+
+    cropx = load_int('cam_cropx')/200
+    cropy = load_int('cam_cropy')/200
+    rotation = load_int('cam_rotation')
+    img = picam2.capture_image()
+
+    if cam_mode !=1:
+        img = img.convert('RGB')
+    w,h = img.size
+
+    if cropx != 0 or cropy != 0:
+        img = img.crop((w*cropx, h*cropy, w * (1-cropx), h * (1-cropy)))
+
+    if rotation == 90:
+        img  = img.transpose(Image.ROTATE_90)
+    elif rotation == 180:
+        img= img.transpose(Image.ROTATE_180)
+    elif rotation == 270:
+        img= img.transpose(Image.ROTATE_270)
+
+    if load_bool("cam_mask"):
+        if cam_mode == 1:
+            downscale = 0.045*1.4
+        else:
+            downscale = 0.1*1.4
+        img = create_mask(img, downscale)
+
+    if load_bool("cam_features") and not load_bool("cam_sharparea"):
+        img = plot_orb_keypoints(img)
+
+    if load_bool("cam_sharparea") and not load_bool("cam_features"):
+        img2 = highlight_sharpest_areas(img)
+        img = overlay_mask(img, img2)
+
+    if cam_mode != 1 and not  load_bool("cam_sharparea") and not load_bool("cam_features"):
+        img = add_histo(img)
+
+    img.save("/home/pi/OpenScan/tmp2/preview.jpg", quality=load_int('cam_jpeg_quality'))
+    print("total " + str(int(1000*(time()-starttime))) + "ms")
+    starttime = time()
+
+    return ({}, 200)
+###################################################################################################################
+@app.route('/picam2_focus', methods=['get'])
+def picam2_focus():
+    focus = float(request.args.get('focus'))
+    picam2.set_controls({"AfMode": 0, "LensPosition": focus})
+    return ({}, 200)
+###################################################################################################################
+@app.route('/picam2_af1', methods=['get'])
+def picam2_af1():
+    from libcamera import controls
+
+    picam2.set_controls({"AfMode": 2 ,"AfTrigger": 0, "AfRange":controls.AfRangeEnum.Macro})
+    return ({}, 200)
+###################################################################################################################
+@app.route('/picam2_af2', methods=['get'])
+def picam2_af2():
+    picam2.set_controls({"AfMode": 2 ,"AfTrigger": 0})
+    return ({}, 200)
+
+###################################################################################################################
+@app.route('/picam2_exposure', methods=['get'])
+def picam2_exposure():
+    exposure = int(request.args.get('exposure'))
+    picam2.controls.AnalogueGain = 1.0
+    picam2.controls.ExposureTime = exposure
+    return ({}, 200)
+###################################################################################################################
+@app.route('/picam2_contrast', methods=['get'])
+def picam2_contrast():
+    contrast = float(request.args.get('contrast'))
+    picam2.controls.Contrast = contrast
+    return ({}, 200)
+###################################################################################################################
+@app.route('/picam2_saturation', methods=['get'])
+def picam2_saturation():
+    saturation = float(request.args.get('saturation'))
+    picam2.controls.Saturation = saturation
+    return ({}, 200)
+###################################################################################################################
+@app.route('/picam2_switch_mode', methods=['get'])
+def picam2_switch_mode():
+    global cam_mode
+    cam_mode = int(request.args.get('mode'))
+    if cam_mode == 1:
+        picam2.switch_mode(capture_config)
+    else:
+        picam2.switch_mode(preview_config)
+    return ({}, 200)
+###################################################################################################################
+@app.route('/picam2_show_mode', methods=['get'])
+def picam2_show_mode():
+    global cam_mode
+    return({"mode":cam_mode},200)
+###################################################################################################################
+@app.route('/picam2_af', methods=['get'])
+def picam2_af():
+    picam2.set_controls({"AfMode": 1 ,"AfTrigger": 0}) # --> wait 3-5s
+    return ({}, 200)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+#    app.run(host='127.0.0.1', port=1312, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=1312, debug=False, threaded=True)
---
+++ update/2024-11S/meanwhile/OpenScan.py
@@ -0,0 +1,316 @@
+basepath = '/home/pi/OpenScan/'
+from os.path import isfile
+import os
+
+def load_bool(name):
+    filename = basepath+'settings/'+name
+    if not isfile(filename):
+        return
+    with open(filename, 'r') as file:
+        value = file.read().replace('\n','')
+    if value == '1' or value == 'True' or value =='true':
+        value = True
+    else:
+        value = False
+    return value
+
+def fade_led(pin_led, fade_steps, duty_max, dir = True):
+    import RPi.GPIO as GPIO
+    import time
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(pin_led, GPIO.OUT)
+    pwm = GPIO.PWM(pin_led, 200)
+
+    if dir:
+        pwm.start(0)
+        for duty_cycle in range(0, fade_steps*10, 1):  # Increase duty cycle in steps
+            pwm.ChangeDutyCycle(duty_max*duty_cycle/(10*fade_steps))
+            time.sleep(0.001)  # Pause between steps (adjust as needed)
+    else:
+        pwm.start(duty_max)
+        for duty_cycle in range(fade_steps*10,0, -1):  # Increase duty cycle in steps
+            pwm.ChangeDutyCycle(duty_max*duty_cycle/(10*fade_steps))
+            time.sleep(0.001)  # Pause between steps (adjust as needed)
+    pwm.stop()
+
+
+def check_hotspot_mode(interface="wlan0"):
+    import subprocess
+    try:
+        output = subprocess.check_output(["iwconfig", interface]).decode("utf-8")
+        if "Mode:Master" in output:
+            return True
+        elif "Mode:Managed" in output:
+            return False
+        else:
+            return False
+    except subprocess.CalledProcessError as e:
+        return False
+
+
+
+def add_wifi_network(ssid, password, country):
+    import re
+    conf_file = "/etc/wpa_supplicant/wpa_supplicant-wlan0.conf"
+
+    if not os.path.exists(conf_file):
+        return False
+
+    if not (ssid and password and country):
+        return False
+
+    with open(conf_file, "r") as f:
+        content = f.read()
+
+    updated_content = re.sub(r'country=\w+', f'country={country}', content)
+
+    if f'ssid="{ssid}"' in content:
+        network_block_pattern = re.compile(
+            r'network=\{\s*ssid="' + re.escape(ssid) + r'".*?psk=".*?".*?\}', re.DOTALL
+        )
+        updated_network_block = f'network={{\n    ssid="{ssid}"\n    psk="{password}"\n    key_mgmt=WPA-PSK\n}}'
+        updated_content = network_block_pattern.sub(updated_network_block, updated_content)
+    else:
+        network_block = f'\nnetwork={{\n    ssid="{ssid}"\n    psk="{password}"\n    key_mgmt=WPA-PSK\n}}\n'
+        updated_content += network_block
+
+    with open(conf_file, "w") as f:
+        f.write(updated_content)
+    os.system("sudo systemctl restart wpa_supplicant@wlan0")
+
+    return True
+
+
+def load_str(name):
+    filename = basepath+'settings/'+name
+    if not isfile(filename):
+        return
+    with open(filename, 'r') as file:
+        value = file.read().replace('\n','')
+    return value
+
+def load_int(name):
+    filename = basepath+'settings/'+name
+    if not isfile(filename):
+        return
+    with open(filename, 'r') as file:
+        value = int(file.read().replace('\n',''))
+    return value
+
+def load_float(name):
+    filename = basepath+'settings/'+name
+    if not isfile(filename):
+        return
+    with open(filename, 'r') as file:
+        value = float(file.read().replace('\n',''))
+    return value
+
+def save(name, value):
+    filename = basepath+'settings/'+name
+    with open(filename, 'w+') as file:
+        file.write(str(value))
+    return
+
+def OpenScanCloud(cmd, msg):
+    from requests import get
+    osc_user = 'openscan'
+    osc_pw = 'free'
+    osc_server = 'http://openscanfeedback.dnsuser.de:1334/'
+
+    try:
+        r = get(osc_server + cmd, auth=(osc_user, osc_pw), params=msg)
+    except:
+        r = type('obj', (object,), {'status_code' : 404, 'text':None})
+    return r
+
+def camera(cmd, msg = {}):
+    from requests import get
+    flask = 'http://127.0.0.1:1312/'
+    try:
+        r = get(flask + cmd, params=msg)
+        return r.status_code
+    except:
+        return 400
+
+def motorrun(motor,angle,ES_enable=False):
+    #motor can be "rotor", "tt" or "extra"
+    import RPi.GPIO as GPIO
+    from time import sleep
+    from math import cos
+    msg = {'cmd':'set'}
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    spr = load_int(motor + '_stepsperrotation')
+    dirpin = load_int('pin_' + motor + '_dir')
+    steppin = load_int('pin_' + motor +'_step')
+    ES_pin = load_int('pin_' + motor + '_endstop')
+    ES_pushed = load_bool(motor + '_endstop_pushed')
+    dir = load_int(motor + '_dir')
+    ramp = load_int(motor + '_accramp')
+    acc = load_float(motor + '_acc')
+    delay_init = load_float(motor + '_delay')
+    delay = delay_init
+
+    step_count=int(angle*spr/360) * dir
+    GPIO.setup(dirpin, GPIO.OUT)
+    GPIO.setup(steppin, GPIO.OUT)
+    GPIO.setup(ES_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+    if (step_count>0):
+        GPIO.output(dirpin, GPIO.HIGH)
+    if(step_count<0):
+        GPIO.output(dirpin, GPIO.LOW)
+        step_count=-step_count
+
+    for x in range(step_count):
+        if ES_enable == True and GPIO.input(ES_pin) == ES_pushed and (motor == "rotor" and GPIO.input(dirpin) == False):
+            i = 0
+            while i <= 10:
+                if GPIO.input(ES_pin) != ES_pushed:
+                    i = 11
+                if i == 10:
+                    return
+                i = i + 1
+
+        GPIO.output(steppin, GPIO.HIGH)
+        if x<=ramp and x<=step_count/2:
+            delay = delay_init * (1 + -1/acc*cos(1*(ramp-x)/ramp)+1/acc)
+            #delay=delay_init+(ramp-x)*(delay_init)/acc
+        elif step_count-x<=ramp and x>step_count/2:
+            delay = delay_init * (1-1/acc*cos(1*(ramp+x-step_count)/ramp)+1/acc)
+            #delay=delay_init+(ramp-step_count+x)*(delay_init)/acc
+        else:
+            delay = delay_init
+        sleep(delay)
+        GPIO.output(steppin, GPIO.LOW)
+        sleep(delay)
+
+def ringlight(number,state):
+    import RPi.GPIO as GPIO
+    msg = {'cmd':'set'}
+    pin = load_int('pin_ringlight' + str(number))
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, state)
+
+def take_photo(file):
+    from os import system
+    filepath = basepath + file
+
+    model=load_str('model')
+
+    shutter = str(load_int('cam_shutter'))
+    saturation = load_str('cam_saturation')
+    contrast = load_str('cam_contrast')
+    awbg_red = load_str('cam_awbg_red')
+    awbg_blue = load_str('cam_awbg_blue')
+    gain = load_str('cam_gain')
+    quality = load_int('cam_jpeg_quality')
+    filepath2 = '/home/pi/OpenScan/tmp/tmp.jpg'
+    #width = load_str('cam_resx')
+    #height = load_str('cam_resy')
+    timeout = load_str('cam_timeout')
+    cropx = load_int('cam_cropx')/200
+    cropy = load_int('cam_cropy')/200
+    rotation = load_int('cam_rotation')
+    AF = load_bool('cam_AFmode')
+    camera = load_str('camera')
+
+
+    if camera == 'imx519' and AF == True:
+        autofocus = ' --autofocus '
+    else:
+        autofocus = ''
+
+    if camera  == "usb_webcam":
+        cmd = 'fswebcam -i 0 -r "1280x720" -F 5 --no-banner --jpeg 95 --save ' + filepath2
+    else:
+        cmd = 'libcamera-still -n --denoise off --sharpness 0 -o ' + filepath2 + ' -t ' + timeout  +' --shutter ' + shutter + ' --saturation ' + saturation + ' --contrast ' + contrast + ' --awbgains '+awbg_red + "," + awbg_blue + ' --gain ' + gain + ' -q ' + str(quality) + autofocus + ' >/dev/null 2>&1'
+    #    cmd = 'libcamera-still -n --denoise off --sharpness 0 -o ' + filepath2 + ' -t ' + timeout  +' --shutter ' + shutter + ' --saturation ' + saturation + ' --contrast ' + contrast + ' --awbgains '+awbg_red + "," + awbg_blue + ' --gain ' + gain + ' -q ' + str(quality) + autofocus
+
+    system(cmd)
+    return cmd
+
+def get_points(samples=1):
+    from math import pi, sqrt, acos, atan2, cos, sin
+
+    points = []
+    phi = pi * (3. - sqrt(5.))
+    for i in range(int(samples)):
+        y = 1 - (i / float(samples - 1)) * 2
+        radius = sqrt(1 - y * y)
+        theta = phi * i
+        x = cos(theta) * radius
+        z = sin(theta) * radius
+        r=sqrt(x*x+y*y+z*z)
+        theta_neu=acos(z/r)*180/pi
+        phi_neu=atan2(y,x)*180/pi
+        points.append((theta_neu-90,phi_neu))
+    points.sort()
+    return points
+
+def create_coordinates(angle_min, angle_max,point_count):
+    point_count_final=point_count
+    if angle_max < angle_min:
+        a = angle_min
+        angle_min = angle_max
+        angle_max = a
+    point_count=point_count*90/(angle_max-angle_min)
+    actual_points=0
+    while actual_points<point_count_final:
+        points=get_points(point_count)
+        filtered=[]
+        for x,y in points:
+            if x>angle_min and x<angle_max and len(filtered)<point_count_final:
+                filtered.append((x,y))
+        actual_points=len(filtered)
+
+        if point_count-actual_points>20:
+            point_count=point_count+3
+        else:
+            point_count=point_count+1
+    return filtered
+
+
+def haversine_distance_deg(theta1, phi1, theta2, phi2):
+    import numpy as np
+    R = 1
+    dtheta = np.radians(theta2 - theta1)
+    dphi = np.radians(phi2 - phi1)
+
+    theta1, phi1 = np.radians(theta1), np.radians(phi1)
+    theta2, phi2 = np.radians(theta2), np.radians(phi2)
+
+    a = np.sin(dtheta / 2) ** 2 + np.cos(theta1) * np.cos(theta2) * np.sin(dphi / 2) ** 2
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+
+    return R * c
+
+def sort_spherical_coordinates_deg(points_spherical_deg):
+    import numpy as np
+    from tsp_solver.greedy import solve_tsp
+
+    points_spherical_deg = np.array(points_spherical_deg)  # Convert list of tuples to NumPy array
+
+    n = len(points_spherical_deg)
+    dist_matrix = np.zeros((n, n))
+
+    # Calculate haversine distance for each pair of points
+    for i in range(n):
+        for j in range(i + 1, n):
+            dist = haversine_distance_deg(points_spherical_deg[i, 0], points_spherical_deg[i, 1],
+                                          points_spherical_deg[j, 0], points_spherical_deg[j, 1])
+            dist_matrix[i, j] = dist
+            dist_matrix[j, i] = dist
+
+    # Solve the TSP problem using the tsp_solver.greedy algorithm
+    path = solve_tsp(dist_matrix)
+
+    sorted_points_spherical_deg = points_spherical_deg[path]
+
+    # Convert the sorted NumPy array back to a list of tuples
+    return [tuple(point) for point in sorted_points_spherical_deg]
