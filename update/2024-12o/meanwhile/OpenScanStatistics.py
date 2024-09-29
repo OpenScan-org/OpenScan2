3018e874c9c54773c390cfa477b531efa635dda0
Stealth <stealth@MacBook-Pro-de-Stealth.local>
2024-09-29 23:01:11 +0200

    u0pdate statistics with openscan versionig

--- update/2024-12o/meanwhile/OpenScanStatistics.py
+++ update/2024-12o/meanwhile/OpenScanStatistics.py
@@ -3,10 +3,10 @@ import csv
 class ScanStatistics:
     def __init__(self, filename="/home/pi/OpenScan/statistics/statistics.csv"):
         self.filename = filename
-        self.header = ["arch", "shield", "date_init", "date_end", "num_photos", "done-photos", "camera", "aborted"]
+        self.header = ["arch", "openscan version", "openscan branch", "shield", "date_init", "date_end", "num_photos", "done-photos", "camera", "aborted"]
 
-    def write_statistics(self, arch, shield, date_init, date_end, num_photos, done_photos, camera, aborted):
-        data = [arch, shield, date_init, date_end, num_photos, done_photos, camera, aborted]
+    def write_statistics(self, arch, openscan_version, openscan_branch, shield, date_init, date_end, num_photos, done_photos, camera, aborted):
+        data = [arch, openscan_version, openscan_branch, shield, date_init, date_end, num_photos, done_photos, camera, aborted]
         
         with open(self.filename, "a", newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=';')
