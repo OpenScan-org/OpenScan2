3febe33d0a8e9b30864b0eb4d7be76793d9ce705
Stealth <stealth@MacBook-Pro-de-Stealth.local>
2024-09-29 19:05:58 +0200

    first 2024-12o version

---
+++ update/2024-12o/stable/OpenScanStatistics.py
@@ -0,0 +1,18 @@
+import csv
+
+class ScanStatistics:
+    def __init__(self, filename="/home/pi/OpenScan/statistics/statistics.csv"):
+        self.filename = filename
+        self.header = ["arch", "shield", "date_init", "date_end", "num_photos", "done-photos", "camera", "aborted"]
+
+    def write_statistics(self, arch, shield, date_init, date_end, num_photos, done_photos, camera, aborted):
+        data = [arch, shield, date_init, date_end, num_photos, done_photos, camera, aborted]
+        
+        with open(self.filename, "a", newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=';')
+            
+            # Write header if file is empty
+            if csv_file.tell() == 0:
+                csv_writer.writerow(self.header)
+            
+            csv_writer.writerow(data)
